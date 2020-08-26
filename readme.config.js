3e0163893debe8604b2c69f40dd54cb6ed7053ec
sahandfarahani <58839234+sahandfarahani@users.noreply.github.com>
2020-08-26 16:53:14 -0400

    Update readme.config.js

--- readme.config.js
+++ readme.config.js
@@ -2,7 +2,7 @@
     "Filter":"ROY",
     "CollapsibleFolder":falase,
     "APPLICATION_NAME":"My APP",
-"IOSStoreLink":"http://google.com",
-  "GooglePlayLink":"http://yahoo.com",
+    "IOSStoreLink":"http://google.com",
+    "GooglePlayLink":"http://yahoo.com",
     "AuthorConfigs":"AuthorInfo":"WithPicture":false,
   }
